bcab7c0e49a0cdb1fc0108cac6fe4434c55a39b9
mmtakeuchi <mikemtaki@gmail.com>
2021-08-23 11:08:48 -0700

    added github link to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Footer.scss";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="copyR">© 2021 Meal Lookup</div>
+      <div className="socials">
+        <Link
+          className="link"
+          to={{
+            pathname: "https://github.com/mmtakeuchi/meals-db",
+          }}
+          target="_blank"
+        >
+          GitHub
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
