4a0292577be52a0ca43b23d39086290623e7cbb9
mmtakeuchi <mikemtaki@gmail.com>
2021-08-25 12:21:18 -0700

    set max width on meal images to be more even when row is not filled

--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -17,15 +17,23 @@ const Meal = ({ type, meal, selectMeal }) => {
       {type === "meal" ? (
         <div className="meal" onClick={handleSelectMeal}>
           <Link to={`/meals/${meal.idMeal}`} className="link">
-            <h2 className="mealName">{meal.strMeal}</h2>
-            <img src={`${meal.strMealThumb}`} alt={meal.strMeal} />
+            <h2 className="mealTitle">{meal.strMeal}</h2>
+            <img
+              className="cardImg"
+              src={`${meal.strMealThumb}`}
+              alt={meal.strMeal}
+            />
           </Link>
         </div>
       ) : (
         <div className="meal" onClick={handleSelectCateogry}>
           <Link to={`/meals/category/${meal.strCategory}`} className="link">
-            <h2 className="mealName">{meal.strCategory}</h2>
-            <img src={meal.strCategoryThumb} alt={meal.strCategory} />
+            <h2 className="mealTitle">{meal.strCategory}</h2>
+            <img
+              className="cardImg"
+              src={meal.strCategoryThumb}
+              alt={meal.strCategory}
+            />
           </Link>
         </div>
       )}
